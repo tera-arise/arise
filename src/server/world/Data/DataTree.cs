51e618e9cbcae5e1c3b1325ee1652a99f6e5348d
Alex Rønne Petersen <alex@alexrp.com>
2023-01-01 08:10:57 +0100

    Add logging to DataTree.
    Closes #3.

--- src/server/world/Data/DataTree.cs
+++ src/server/world/Data/DataTree.cs
@@ -1,20 +1,27 @@
 namespace Arise.Server.World.Data;
 
-public sealed class DataTree : IHostedService
+public sealed partial class DataTree : IHostedService
 {
+    private static partial class Log
+    {
+        [LoggerMessage(0, LogLevel.Information, "Loaded embedded data center as {Mode} in {ElapsedMs:0.0000} ms")]
+        public static partial void LoadedDataCenter(ILogger logger, DataCenterLoaderMode mode, double elapsedMs);
+    }
+
     public DataCenterNode Root { get; private set; } = null!;
 
     private readonly IHostEnvironment _environment;
 
-    public DataTree(IHostEnvironment environment)
+    private readonly ILogger<DataTree> _logger;
+
+    public DataTree(IHostEnvironment environment, ILogger<DataTree> logger)
     {
         _environment = environment;
+        _logger = logger;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        // TODO: Add logging.
-
         await using var stream = EmbeddedDataCenter.OpenStream();
         var attrs = typeof(ThisAssembly).Assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
 
@@ -23,15 +30,20 @@ public sealed class DataTree : IHostedService
             return Convert.FromHexString(attrs.Single(attr => attr.Key == key).Value!);
         }
 
+        var mode = _environment.IsDevelopment() ? DataCenterLoaderMode.Lazy : DataCenterLoaderMode.Eager;
+        var stamp = Stopwatch.GetTimestamp();
+
         Root = await DataCenter.LoadAsync(
             stream,
             new DataCenterLoadOptions()
                 .WithKey(GetByteArray("DataCenterKey"))
                 .WithIV(GetByteArray("DataCenterIV"))
                 .WithStrict(true)
-                .WithLoaderMode(_environment.IsDevelopment() ? DataCenterLoaderMode.Lazy : DataCenterLoaderMode.Eager)
+                .WithLoaderMode(mode)
                 .WithMutability(DataCenterMutability.Immutable),
             cancellationToken);
+
+        Log.LoadedDataCenter(_logger, mode, Stopwatch.GetElapsedTime(stamp).TotalMilliseconds);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
