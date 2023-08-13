edc13bc55ce85732f89109723cdf9d2862bea9f3
Alex Rønne Petersen <alex@alexrp.com>
2023-08-13 02:05:04 +0200

    Don't conditionalize DaemonEnvironment.{Production,Staging}.

--- src/server/daemon/DaemonEnvironment.cs
+++ src/server/daemon/DaemonEnvironment.cs
@@ -3,10 +3,7 @@ namespace Arise.Server.Daemon;
 // These names must match Microsoft.Extensions.Hosting.Environments.
 public enum DaemonEnvironment
 {
-#if DEBUG
     Development,
-#else
     Staging,
     Production,
-#endif
 }
