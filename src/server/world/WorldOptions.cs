c48e9015fee04e8f5dbd0302c770f3bf08c9af1e
Alex Rønne Petersen <alex@alexrp.com>
2023-08-14 10:58:50 +0200

    Tweak default bridge module provisioning settings slightly.

--- src/server/world/WorldOptions.cs
+++ src/server/world/WorldOptions.cs
@@ -2,11 +2,11 @@ namespace Arise.Server;
 
 public sealed class WorldOptions : IOptions<WorldOptions>
 {
-    public int ConcurrentModules { get; } = 10;
+    public int ConcurrentModules { get; } = 1;
 
     public Duration ModuleRotationTime { get; } = Duration.FromDays(1);
 
-    public Duration ModuleValidityTime { get; } = Duration.FromDays(7);
+    public Duration ModuleValidityTime { get; } = Duration.FromDays(2);
 
     public ICollection<string> Endpoints { get; } = new List<string>();
 
