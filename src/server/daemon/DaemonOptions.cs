3545c010c8e35672f64aca436ec04ada62b40bb7
Alex Rønne Petersen <alex@alexrp.com>
2023-08-24 06:28:16 +0200

    Explicitly set DaemonOptions.Environment's default to Development.

--- src/server/daemon/DaemonOptions.cs
+++ src/server/daemon/DaemonOptions.cs
@@ -3,7 +3,11 @@ namespace Arise.Server.Daemon;
 [SuppressMessage("", "CA1812")]
 internal sealed class DaemonOptions
 {
-    [Option('e', "environment", HelpText = "Deployment environment. (Development, Staging, Production)")]
+    [Option(
+        'e',
+        "environment",
+        Default = DaemonEnvironment.Development,
+        HelpText = "Deployment environment. (Development, Staging, Production)")]
     public required DaemonEnvironment Environment { get; init; }
 
     [Option('s', "services", Default = DaemonServices.All, HelpText = "Services to run. (Web, World)")]
