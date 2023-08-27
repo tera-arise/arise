tera-arise/arise

commit fa3a0b24885b3f1f9084ceb23081213c2712ac6a

Expose a BridgeProtectionComponent from the bridge module.
Allows starting/stopping the protection tasks.

src/server/world/Bridge/BridgeModulePatchingPass.cs
@@ -71,7 +71,9 @@ internal sealed class BridgeModulePatchingPass : BridgeModulePass
 
             switch ((method.DeclaringType.Name.String, method.Name.String))
             {
-                case ("GameProtection", "Initialize") when kind == BridgeModuleKind.Normal:
+                case ("GameProtection", "Initialize") or
+                     ("PatchableBridgeProtectionComponent", "Start") or
+                     ("PatchableBridgeProtectionComponent", "Stop") when kind == BridgeModuleKind.Normal:
                 {
                     insns.Clear();
                     insns.Add(Instruction.Create(OpCodes.Ret));

src/shared/module/ModuleInitializer.cs
@@ -1,0 +1,13 @@
+using Arise.Bridge.Protection;
+
+namespace Arise.Bridge;
+
+internal static class ModuleInitializer
+{
+    [ModuleInitializer]
+    [SuppressMessage("", "CA2255")]
+    public static void Initialize()
+    {
+        GameProtection.Initialize();
+    }
+}

src/shared/module/PatchableBridgeModule.cs
@@ -1,17 +1,12 @@
-using Arise.Bridge.Protection;
-
 namespace Arise.Bridge;
 
 public sealed class PatchableBridgeModule : BridgeModule
 {
-    public override PatchableBridgeDataComponent Data { get; } = new PatchableBridgeDataComponent();
+    public override PatchableBridgeDataComponent Data { get; } = new();
 
-    public override PatchableBridgeProtocolComponent Protocol { get; } = new PatchableBridgeProtocolComponent();
+    public override PatchableBridgeProtectionComponent Protection { get; } = new();
 
-    public override PatchableBridgeWatchdogComponent Watchdog { get; } = new PatchableBridgeWatchdogComponent();
+    public override PatchableBridgeProtocolComponent Protocol { get; } = new();
 
-    public PatchableBridgeModule()
-    {
-        GameProtection.Initialize();
-    }
+    public override PatchableBridgeWatchdogComponent Watchdog { get; } = new();
 }

src/shared/module/PatchableBridgeProtectionComponent.cs
@@ -1,0 +1,32 @@
+using Arise.Bridge.Protection;
+
+namespace Arise.Bridge;
+
+public sealed class PatchableBridgeProtectionComponent : BridgeProtectionComponent
+{
+    private readonly IEnumerable<GameProtectionTask> _tasks = new GameProtectionTask[]
+    {
+        new CodeChecksumTask(),
+        new DebuggerDetectionTask(),
+    };
+
+    [Obfuscation]
+    public override void Start()
+    {
+        // This method body is erased by the server's BridgeModuleGenerator for module instances that run on the server
+        // and for module instances running on the client in development scenarios.
+
+        foreach (var task in _tasks)
+            task.Start();
+    }
+
+    [Obfuscation]
+    public override void Stop()
+    {
+        // This method body is erased by the server's BridgeModuleGenerator for module instances that run on the server
+        // and for module instances running on the client in development scenarios.
+
+        foreach (var task in _tasks.Reverse())
+            task.Stop();
+    }
+}

src/shared/module/Protection/GameProtection.cs
@@ -4,24 +4,16 @@ namespace Arise.Bridge.Protection;
 
 internal static class GameProtection
 {
-    private static int _initialized;
-
     [Obfuscation]
     public static void Initialize()
     {
         // This method body is erased by the server's BridgeModuleGenerator for module instances that run on the server
         // and for module instances running on the client in development scenarios.
 
-        if (Interlocked.Exchange(ref _initialized, 1) == 1)
-            return;
-
         var culture = CultureInfo.InvariantCulture;
 
         if (DateTime.UtcNow - DateTime.Parse(GetIssueTime(), culture) > TimeSpan.Parse(GetValidDuration(), culture))
             Terminate();
-
-        new CodeChecksumTask().Start();
-        new DebuggerDetectionTask().Start();
     }
 
     [Obfuscation]

src/shared/module/Protection/GameProtectionTask.cs
@@ -1,29 +1,36 @@
 namespace Arise.Bridge.Protection;
 
+[SuppressMessage("", "CA1001")]
 internal abstract class GameProtectionTask
 {
-    private static volatile bool _unloading;
+    private readonly TaskCompletionSource _ready = new(TaskCreationOptions.RunContinuationsAsynchronously);
 
-    static GameProtectionTask()
+    private readonly CancellationTokenSource _cts = new();
+
+    private readonly TaskCompletionSource _checkDone = new(TaskCreationOptions.RunContinuationsAsynchronously);
+
+    protected GameProtectionTask()
     {
-        // Make sure we exit when the client/server drops all references to us.
-        AssemblyLoadContext.GetLoadContext(typeof(ThisAssembly).Assembly)!.Unloading += static _ => _unloading = true;
+        var ct = _cts.Token;
+
+        _ = Task.Run(() => PerformCheckAsync(ct), ct);
     }
 
     public void Start()
     {
-        _ = Task.Run(async () =>
-        {
-            Initialize();
+        _ = _ready.TrySetResult();
+    }
 
-            while (!_unloading)
-            {
-                if (!Check())
-                    GameProtection.Terminate();
+    public void Stop()
+    {
+        // Signal the check task to shut down.
+        _cts.Cancel();
 
-                await Task.Delay(GetCheckInterval()).ConfigureAwait(false);
-            }
-        });
+        // Note that the check task is not expected to encounter any exceptions.
+        _checkDone.Task.GetAwaiter().GetResult();
+
+        // The task is gone; safe to dispose this now.
+        _cts.Dispose();
     }
 
     protected virtual void Initialize()
@@ -37,5 +44,18 @@ internal abstract class GameProtectionTask
     {
         // Filled in by the server's BridgeModuleGenerator.
         return 42;
+    }
+
+    private async Task PerformCheckAsync(CancellationToken cancellationToken)
+    {
+        Initialize();
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            if (!Check())
+                GameProtection.Terminate();
+
+            await Task.Delay(GetCheckInterval(), cancellationToken).ConfigureAwait(false);
+        }
     }
 }

src/client/symbiote/GameApplicationHost.cs
@@ -1,17 +1,25 @@
+using Arise.Client.Net;
+
 namespace Arise.Client;
 
 [SuppressMessage("", "CA1812")]
 internal sealed class GameApplicationHost : IHostedService
 {
+    private readonly BridgeProtectionComponent _protectionComponent;
+
     private readonly Action _wake;
 
-    public GameApplicationHost(Action wake)
+    public GameApplicationHost(GameClient client, Action wake)
     {
+        _protectionComponent = client.Session.Module.Protection;
         _wake = wake;
     }
 
     Task IHostedService.StartAsync(CancellationToken cancellationToken)
     {
+        // Start watching for code changes, debuggers, etc.
+        _protectionComponent.Start();
+
         // Wake up the game now that we are fully initialized.
         _wake();
 
@@ -20,6 +28,9 @@ internal sealed class GameApplicationHost : IHostedService
 
     Task IHostedService.StopAsync(CancellationToken cancellationToken)
     {
+        // Stop the protection tasks so we can undo our code changes.
+        _protectionComponent.Stop();
+
         return Task.CompletedTask;
     }
 }

src/shared/core/Bridge/BridgeModule.cs
@@ -4,6 +4,8 @@ public abstract class BridgeModule
 {
     public abstract BridgeDataComponent Data { get; }
 
+    public abstract BridgeProtectionComponent Protection { get; }
+
     public abstract BridgeProtocolComponent Protocol { get; }
 
     public abstract BridgeWatchdogComponent Watchdog { get; }

commit 9efea4643fdcf5664e29b50ec5f4b96f5cdbcc15

Fix bugs introduced in fa3a0b24885b3f1f9084ceb23081213c2712ac6a

src/server/world/Bridge/BridgeModuleGenerator.cs
@@ -90,7 +90,7 @@ internal sealed partial class BridgeModuleGenerator : IHostedService
                 {
                     _modules.Clear();
 
-                    var serverKind = _hostEnvironment.IsDevelopment()
+                    var clientKind = _hostEnvironment.IsDevelopment()
                         ? BridgeModuleKind.Normal
                         : BridgeModuleKind.Hardened;
 
@@ -100,7 +100,7 @@ internal sealed partial class BridgeModuleGenerator : IHostedService
 
                         _modules.Add(
                             (BridgeModuleActivator.Create(CreateModule(BridgeModuleKind.Normal, seed)),
-                             CreateModule(serverKind, seed)));
+                             CreateModule(clientKind, seed)));
                     }
                 }
 

src/server/world/Bridge/BridgeModulePatchingPass.cs
@@ -69,11 +69,9 @@ internal sealed class BridgeModulePatchingPass : BridgeModulePass
                 insns.Add(Instruction.Create(OpCodes.Ret));
             }
 
-            switch ((method.DeclaringType.Name.String, method.Name.String))
+            switch (((string)method.DeclaringType.Name, (string)method.Name))
             {
-                case ("GameProtection", "Initialize") or
-                     ("PatchableBridgeProtectionComponent", "Start") or
-                     ("PatchableBridgeProtectionComponent", "Stop") when kind == BridgeModuleKind.Normal:
+                case ("GameProtection", "Initialize") when kind == BridgeModuleKind.Normal:
                 {
                     insns.Clear();
                     insns.Add(Instruction.Create(OpCodes.Ret));

src/shared/module/ModuleInitializer.cs
@@ -1,13 +1,0 @@
-using Arise.Bridge.Protection;
-
-namespace Arise.Bridge;
-
-internal static class ModuleInitializer
-{
-    [ModuleInitializer]
-    [SuppressMessage("", "CA2255")]
-    public static void Initialize()
-    {
-        GameProtection.Initialize();
-    }
-}

src/shared/module/PatchableBridgeModule.cs
@@ -1,3 +1,5 @@
+using Arise.Bridge.Protection;
+
 namespace Arise.Bridge;
 
 public sealed class PatchableBridgeModule : BridgeModule
@@ -9,4 +11,9 @@ public sealed class PatchableBridgeModule : BridgeModule
     public override PatchableBridgeProtocolComponent Protocol { get; } = new();
 
     public override PatchableBridgeWatchdogComponent Watchdog { get; } = new();
+
+    public PatchableBridgeModule()
+    {
+        GameProtection.Initialize();
+    }
 }

src/shared/module/PatchableBridgeProtectionComponent.cs
@@ -4,28 +4,19 @@ namespace Arise.Bridge;
 
 public sealed class PatchableBridgeProtectionComponent : BridgeProtectionComponent
 {
-    private readonly IEnumerable<GameProtectionTask> _tasks = new GameProtectionTask[]
-    {
-        new CodeChecksumTask(),
-        new DebuggerDetectionTask(),
-    };
+    private readonly Queue<GameProtectionTask> _tasks = new();
 
-    [Obfuscation]
     public override void Start()
     {
-        // This method body is erased by the server's BridgeModuleGenerator for module instances that run on the server
-        // and for module instances running on the client in development scenarios.
+        _tasks.Enqueue(new CodeChecksumTask());
+        _tasks.Enqueue(new DebuggerDetectionTask());
 
         foreach (var task in _tasks)
             task.Start();
     }
 
-    [Obfuscation]
     public override void Stop()
     {
-        // This method body is erased by the server's BridgeModuleGenerator for module instances that run on the server
-        // and for module instances running on the client in development scenarios.
-
         foreach (var task in _tasks.Reverse())
             task.Stop();
     }

src/shared/module/Protection/GameProtection.cs
@@ -4,11 +4,16 @@ namespace Arise.Bridge.Protection;
 
 internal static class GameProtection
 {
+    private static int _initialized;
+
     [Obfuscation]
     public static void Initialize()
     {
         // This method body is erased by the server's BridgeModuleGenerator for module instances that run on the server
         // and for module instances running on the client in development scenarios.
+
+        if (Interlocked.Exchange(ref _initialized, 1) == 1)
+            return;
 
         var culture = CultureInfo.InvariantCulture;
 

src/shared/module/Protection/GameProtectionTask.cs
@@ -48,6 +48,8 @@ internal abstract class GameProtectionTask
 
     private async Task PerformCheckAsync(CancellationToken cancellationToken)
     {
+        await _ready.Task.ConfigureAwait(false);
+
         Initialize();
 
         while (!cancellationToken.IsCancellationRequested)