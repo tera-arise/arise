796ed0f8629cc4b1d2b7f563516bfe74bdaa5b0d
Alex Rønne Petersen <alex@alexrp.com>
2023-08-27 20:01:21 +0200

    Some styling of the launcher window.

--- src/client/launcher/Controllers/MainController.cs
+++ src/client/launcher/Controllers/MainController.cs
@@ -2,6 +2,24 @@ namespace Arise.Client.Launcher.Controllers;
 
 internal sealed class MainController : LauncherController
 {
+    public Bitmap Background => _background.Value;
+
+    [SuppressMessage("", "CA5394")]
+    private readonly Lazy<Bitmap> _background = new(
+        static () =>
+        {
+            var candidates = AssetLoader
+                .GetAssets(new($"avares://{ThisAssembly.AssemblyName}/assets"), null)
+                .Where(static uri =>
+                    Path.GetFileName(uri.AbsolutePath).StartsWith("background_", StringComparison.Ordinal))
+                .ToArray();
+
+            using var stream = AssetLoader.Open(candidates[Random.Shared.Next() % candidates.Length]);
+
+            return new(stream);
+        },
+        isThreadSafe: false);
+
     public MainController(IServiceProvider services)
         : base(services)
     {
