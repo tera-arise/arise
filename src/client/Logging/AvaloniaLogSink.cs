99e8763d8877d3c4a62fc8df78c82d0190820781
Alex Rønne Petersen <alex@alexrp.com>
2024-01-23 14:03:27 +0100

    Use Vezel.Cathode.Extensions instead of Serilog for logging in the client.
    This is smaller and more trimming-friendly than Serilog, and we don't actually
    need all the fluff that Serilog brings.

--- src/client/Logging/AvaloniaLogSink.cs
+++ src/client/Logging/AvaloniaLogSink.cs
@@ -6,29 +6,25 @@ internal sealed class AvaloniaLogSink : ILogSink
 {
     private readonly ILoggerFactory _loggerFactory;
 
-    private readonly ConcurrentDictionary<string, Microsoft.Extensions.Logging.ILogger> _loggers = new();
+    private readonly ConcurrentDictionary<string, ILogger> _loggers = new();
 
     public AvaloniaLogSink(ILoggerFactory loggerFactory)
     {
         _loggerFactory = loggerFactory;
     }
 
-    public bool IsEnabled(Avalonia.Logging.LogEventLevel level, string area)
+    public bool IsEnabled(LogEventLevel level, string area)
     {
         return true;
     }
 
-    public void Log(Avalonia.Logging.LogEventLevel level, string area, object? source, string messageTemplate)
+    public void Log(LogEventLevel level, string area, object? source, string messageTemplate)
     {
         Log(level, area, source, messageTemplate, []);
     }
 
     public void Log(
-        Avalonia.Logging.LogEventLevel level,
-        string area,
-        object? source,
-        string messageTemplate,
-        params object?[] propertyValues)
+        LogEventLevel level, string area, object? source, string messageTemplate, params object?[] propertyValues)
     {
         var logger = _loggers.GetOrAdd(
             source?.GetType()?.FullName ?? $"Avalonia.{area}",
@@ -36,12 +32,12 @@ internal sealed class AvaloniaLogSink : ILogSink
             _loggerFactory);
         var logLevel = level switch
         {
-            Avalonia.Logging.LogEventLevel.Verbose => LogLevel.Trace,
-            Avalonia.Logging.LogEventLevel.Debug => LogLevel.Debug,
-            Avalonia.Logging.LogEventLevel.Information => LogLevel.Information,
-            Avalonia.Logging.LogEventLevel.Warning => LogLevel.Warning,
-            Avalonia.Logging.LogEventLevel.Error => LogLevel.Error,
-            Avalonia.Logging.LogEventLevel.Fatal => LogLevel.Critical,
+            LogEventLevel.Verbose => LogLevel.Trace,
+            LogEventLevel.Debug => LogLevel.Debug,
+            LogEventLevel.Information => LogLevel.Information,
+            LogEventLevel.Warning => LogLevel.Warning,
+            LogEventLevel.Error => LogLevel.Error,
+            LogEventLevel.Fatal => LogLevel.Critical,
             _ => throw new UnreachableException(),
         };
 
--- src/client/Program.cs
+++ src/client/Program.cs
@@ -25,11 +25,6 @@ internal static class Program
         if (isLauncher)
             _ = AttachConsole(ATTACH_PARENT_PROCESS);
 
-        Log.Logger = new LoggerConfiguration()
-            .Enrich.FromLogContext()
-            .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
-            .CreateBootstrapLogger();
-
         return new HostBuilder()
             .ConfigureAppConfiguration(builder => builder.AddCommandLine(args))
             .UseDefaultServiceProvider(static opts =>
@@ -75,22 +70,14 @@ internal static class Program
                         .Services
                         .AddHostedService(static provider => provider.GetRequiredService<LauncherApplicationHost>());
             })
-            .UseSerilog((ctx, services, cfg) =>
+            .ConfigureLogging(cfg =>
             {
                 // Make game logging output available in a new console window if requested.
-                if (!isLauncher && services.GetRequiredService<IOptions<GameOptions>>().Value.Console)
+                if (!isLauncher &&
+                    cfg.Services.BuildServiceProvider().GetRequiredService<IOptions<GameOptions>>().Value.Console)
                     _ = AllocConsole();
 
-                _ = cfg
-                    .MinimumLevel.Is(Serilog.Events.LogEventLevel.Information)
-                    .Enrich.FromLogContext()
-                    .WriteTo.Console(
-                        outputTemplate:
-                            "[{Timestamp:HH:mm:ss}][{Level:w3}][{SourceContext}] {Message:lj}{NewLine}{Exception}",
-                        formatProvider: CultureInfo.InvariantCulture,
-                        standardErrorFromLevel: Serilog.Events.LogEventLevel.Warning,
-                        theme: AnsiConsoleTheme.Code)
-                    .ReadFrom.Services(services);
+                _ = cfg.AddTerminal(static opts => opts.LogToStandardErrorThreshold = LogLevel.Warning);
             })
             .RunConsoleAsync();
     }
