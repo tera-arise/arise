7c76427cc8c3b63a0194a21207b2da51dfea5742
Foglio <foglio1024@gmail.com>
2024-01-23 11:31:04 +0100

    fix designer process crashing due to null DataContext

--- src/client/Launcher/Windows/MainWindow.axaml.cs
+++ src/client/Launcher/Windows/MainWindow.axaml.cs
@@ -12,8 +12,11 @@ public sealed partial class MainWindow : LauncherWindow<MainController>
 
     protected override void OnInitialized()
     {
-        Opened += (_, _) => DataContext.PlayMusic();
-        Closed += (_, _) => DataContext.StopMusic();
+        if (!Design.IsDesignMode)
+        {
+            Opened += (_, _) => DataContext.PlayMusic();
+            Closed += (_, _) => DataContext.StopMusic();
+        }
 
         base.OnInitialized();
     }
