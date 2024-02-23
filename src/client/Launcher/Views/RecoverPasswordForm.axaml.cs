8c8bbeda91ed4e9f231a95e58389cc06b9b8777f
Foglio <foglio1024@gmail.com>
2024-02-24 00:03:17 +0100

    focus the first textbox when showing modals

--- src/client/Launcher/Views/RecoverPasswordForm.axaml.cs
+++ src/client/Launcher/Views/RecoverPasswordForm.axaml.cs
@@ -1,4 +1,5 @@
-using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace Arise.Client.Launcher.Views;
 
@@ -7,5 +8,13 @@ public partial class RecoverPasswordForm : UserControl
     public RecoverPasswordForm()
     {
         InitializeComponent();
+    }
+
+    private void EmailTextBox_OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        if (sender is InputElement i)
+        {
+            _ = i.Focus();
+        }
     }
 }
