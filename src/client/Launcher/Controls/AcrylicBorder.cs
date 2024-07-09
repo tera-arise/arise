33e3a78c587fbbabe82214f0b6445cf5fd95300c
Foglio <foglio1024@gmail.com>
2024-07-09 21:28:36 +0200

    add custom AcrylicBorder control

---
+++ src/client/Launcher/Controls/AcrylicBorder.cs
@@ -0,0 +1,25 @@
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace Arise.Client.Launcher.Controls;
+
+public class AcrylicBorder : TemplatedControl
+{
+    public static readonly StyledProperty<BoxShadows> BoxShadowProperty =
+        AvaloniaProperty.Register<AcrylicBorder, BoxShadows>(nameof(BoxShadow));
+
+    public static readonly StyledProperty<ExperimentalAcrylicMaterial> MaterialProperty =
+        AvaloniaProperty.Register<AcrylicBorder, ExperimentalAcrylicMaterial>(nameof(Material));
+
+    public BoxShadows BoxShadow
+    {
+        get => GetValue(BoxShadowProperty);
+        set => SetValue(BoxShadowProperty, value);
+    }
+
+    public ExperimentalAcrylicMaterial Material
+    {
+        get => GetValue(MaterialProperty);
+        set => SetValue(MaterialProperty, value);
+    }
+}
