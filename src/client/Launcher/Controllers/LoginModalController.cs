5ece365501f3984d230963165e3bb33b9d22cf4c
Foglio <foglio1024@gmail.com>
2024-07-09 21:28:37 +0200

    refactor modal classes to allow multiple modal templates

---
+++ src/client/Launcher/Controllers/LoginModalController.cs
@@ -0,0 +1,92 @@
+#define FORCE_LOGIN
+
+using Arise.Client.Gateway;
+using Arise.Client.Launcher.Settings;
+
+namespace Arise.Client.Launcher.Controllers;
+
+internal sealed partial class LoginModalController : ModalController
+{
+    private readonly GatewayClient _gatewayClient;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+    private string _email = string.Empty;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+    private string _password = string.Empty;
+
+    [ObservableProperty]
+    private bool _rememberMe;
+
+    private bool CanExecuteLogin => !string.IsNullOrEmpty(Email)
+                                 && !string.IsNullOrEmpty(Password);
+
+    public LoginModalController(IServiceProvider services, MainController mainController)
+        : base(services, mainController)
+    {
+        _gatewayClient = Services.GetService<GatewayClient>()!;
+        var settingsManager = Services.GetService<LauncherSettingsManager>()!;
+        _gatewayClient.BaseAddress = settingsManager.Settings.ServerAddress;
+    }
+
+    [RelayCommand(CanExecute = nameof(CanExecuteLogin))]
+    private async Task LoginAsync()
+    {
+        // todo: add an IsLoggingIn state to signal the user that the request is being processed
+        // maybe bind it to a spinning indicator
+
+        // todo: use RememberMe setting (save it locally?)
+
+        if (!MainController.IsLoggedIn)
+        {
+            // todo: catch something?
+            try
+            {
+#if FORCE_LOGIN // find a better way of doing this when you don't have a backend
+                if (await Task.FromResult(true).ConfigureAwait(true))
+#else
+                var resp = await _gatewayClient.Rest
+                    .AuthenticateAccountAsync(Email, Password).ConfigureAwait(true);
+
+                if (resp.IsSuccessStatusCode)
+#endif
+                {
+                    MainController.IsLoggedIn = true; // todo
+                    MainController.CurrentAccountName = Email; // todo
+
+                    // todo: handle all the data in the response (ban, mail verification, etc)
+                }
+                else
+                {
+                    // todo: handle login fail
+                }
+            }
+            finally
+            {
+                // clear the password as soon as it's been sent
+                Password = string.Empty;
+
+                // IsModalVisible = false; // todo
+            }
+        }
+        else
+        {
+            // todo: warn?
+        }
+    }
+
+    [RelayCommand]
+    [SuppressMessage("", "CA1822")]
+    private void RecoverPassword()
+    {
+        // todo
+    }
+
+    [RelayCommand]
+    private void Register()
+    {
+        MainController.CurrentModalController = new RegistrationModalController(Services, MainController);
+    }
+}
---
+++ src/client/Launcher/Views/RegistrationForm.axaml.cs
@@ -0,0 +1,9 @@
+namespace Arise.Client.Launcher.Views;
+
+public partial class RegistrationForm : UserControl
+{
+    public RegistrationForm()
+    {
+        InitializeComponent();
+    }
+}
