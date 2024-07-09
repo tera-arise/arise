4ec5dbe0f2caf73f418dcafc16bd095e4021310b
Foglio <foglio1024@gmail.com>
2024-07-09 21:28:37 +0200

    fix error when trying to initialize the same WasapiOut multiple times

--- src/client/Launcher/Media/MusicPlayer.cs
+++ src/client/Launcher/Media/MusicPlayer.cs
@@ -6,6 +6,8 @@ public sealed class MusicPlayer : IDisposable
 {
     private readonly WasapiOut _wasapi = new();
 
+    private bool _isInitialized;
+
     [SuppressMessage("", "CA1063")]
     void IDisposable.Dispose()
     {
@@ -15,10 +17,15 @@ public sealed class MusicPlayer : IDisposable
     [SuppressMessage("", "CA2000")]
     public void Play()
     {
-        _wasapi.Init(
-            new LoopStream(
-                new WaveChannel32(
-                    new VorbisWaveReader(EmbeddedMediaAssets.Open("audio_launcher.ogg")), volume: 0.1f, pan: 0.0f)));
+        if (!_isInitialized)
+        {
+            _wasapi.Init(
+                new LoopStream(
+                    new WaveChannel32(
+                        new VorbisWaveReader(EmbeddedMediaAssets.Open("audio_launcher.ogg")), volume: 0.1f, pan: 0.0f)));
+
+            _isInitialized = true;
+        }
 
         _wasapi.Play();
     }
